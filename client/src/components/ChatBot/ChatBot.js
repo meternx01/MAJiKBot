5f697a22bcae835530a369809e1116ae9bf369fa
MANOJ BILTHARE <kriti.tiwari28@gmail.com>
2017-12-04 01:13:39 -0600

    added functionalities to the chat bot

--- client/src/components/ChatBot/ChatBot.js
+++ client/src/components/ChatBot/ChatBot.js
@@ -10,10 +10,36 @@ class Chatbot extends Component {
       <ChatBot className="chatbot-whole"
         steps={[
           {
-            id: 'hello-world',
-            message: 'Hello World!',
-            end: true,
-          },
+            id: '1',
+      message: 'What is your name?',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Hi {previousValue}, nice to meet you!',
+      trigger: '4',
+    },
+    {
+      id: '4',
+      message: 'How are you',
+      trigger: '5',
+    },
+    {
+      id: '5',
+      user: true,
+      trigger:'6',
+    },
+    {
+      id: '6',
+      message: 'Great',
+      end:true,
+    },
+
         ]}
       />
 
